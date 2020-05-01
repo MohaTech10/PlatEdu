51c29486e26666f15d9f7bdaf58b994ba4a21bfb
MohaPy <moody54324@gmail.com>
2020-05-01 23:12:39 +0300

    initial

--- MohTech/admin.py
+++ MohTech/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+admin.site.register(Courses)
+admin.site.register(Student)
+admin.site.register(Projects)
+admin.site.register(Comments)
+admin.site.register(Lessons)
+admin.site.register(Sections)
+admin.site.register(CoursesOrdered)
+admin.site.register(Orders)
+admin.site.register(Review)
